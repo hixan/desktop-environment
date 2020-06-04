e84ba325b0c78b59ed8b784c6c9f3cf53691d759
Alex Elias <alex.88.elias@gmail.com>
2020-06-04 20:58:06 +1000

    initial commit

---
+++ compton/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# kill previous instances
+killall -q compton
+
+# wait for them to shut down completely
+while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+
+compton --config $HOME/.config/regolith/compton/config &
+
+echo "Compton launched..." >> $HOME/test.txt
