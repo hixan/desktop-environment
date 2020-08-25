7509777b288e8f8645ad53e8db69caa87d10b716
Hixan <alex.88.elias@gmail.com>
2020-08-25 11:19:25 +1000

    modify location of metadata

--- scripts/python_tools.py
+++ scripts/python_tools.py
@@ -1,14 +1,24 @@
 from subprocess import Popen, PIPE
 from pathlib import Path
+import os
 
-def get_field(response, cut=-1, skiplines=0):
+DIR = Path(os.getenv('HOME'))/'.config'/'desktop-environment'
+METADIR = DIR/'meta'
+SCRIPTSDIR = DIR / 'scripts'
+
+def get_field(response: Popen, cut: int=-1, skiplines: int=0):
     return response.stdout.readlines()[skiplines].decode('utf-8').strip().split()[cut]
 
 def get_active_pid():
     wid = get_field(Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=PIPE))
     return str(int(get_field(Popen(['xprop', '-id', wid, '_NET_WM_PID'], stdout=PIPE))) + 1)
+
+def print_output(pcs: Popen):
+    print(pcs.stdout.read())
+    return pcs
     
-def get_tty(pid):
+def get_tty(pid: str):
+    print(pid)
     if (rv := get_field(Popen(['ps', '-q', pid, 'axo', 'tty'], stdout=PIPE), skiplines=1)) == '?':
         return False
     return rv
@@ -19,6 +29,14 @@ def get_active_wd():
     if symlink.exists():
         return get_field(Popen(['readlink', str(symlink)], stdout=PIPE))
     raise RuntimeError('Could not retrieve active workspace')
+
+def is_python_file(file: Path):
+    assert file.exists(), f'file "{file.absolute()}" does not exist'
+    if file.suffix == '.py':
+        return True
+    # check for hashbang
+    if (hb := file.open('r').readline())[0:2] == '#!':
+        return 'python' in hb
 
 if __name__ == '__main__':
     print(get_active_pid())
